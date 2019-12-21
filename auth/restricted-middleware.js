9748b8f7a9be9901f864714d10b013fb0fe4e4c9
Jacqueline Omollo <jacquelineOmolloo@gmail.com>
2019-12-21 15:55:34 -0500

    migration and file created

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const secrets = require("../database/dbConfig");
+const secrets = require("../config/dbConfig");
 
 module.exports = (req, res, next) => {
   const token = req.headers.authorization;
--- knexfile.js
+++ knexfile.js
@@ -3,15 +3,9 @@
 module.exports = {
   development: {
     client: "sqlite3",
+    useNullAsDefault: true,
     connection: {
       filename: "./database/dev.sqlite3"
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: "./database/migrations"
-    },
-    seeds: {
-      directory: "./database/seeds"
     },
     pool: {
       afterCreate: (connection, done) => {
@@ -19,14 +13,17 @@ module.exports = {
       }
     }
   },
+  migrations: {
+    directory: "./database/migrations"
+  },
+  seeds: {
+    directory: "./database/seeds"
+  },
 
   production: {
     client: "pg",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
+    connection: process.env.DATABASE_URL,
+
     pool: {
       min: 2,
       max: 10

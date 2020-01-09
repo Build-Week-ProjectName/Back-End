732e7f331dda260696ae95d17d5c8591b0d66875
Jacqueline Omollo <jacquelineOmolloo@gmail.com>
2020-01-08 22:59:52 -0500

    id change for users seeds

--- database/seeds/001.users.js
+++ database/seeds/001.users.js
@@ -6,24 +6,28 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex("users").insert([
         {
+          id: 1,
           username: "bill",
           password: "password",
           email: "bill@gmail.com",
           role: "operator"
         },
         {
+          id: 2,
           username: "tom",
           password: "password",
           email: "tom@gmail.com",
           role: "diner"
         },
         {
+          id: 3,
           username: "jack",
           password: "password",
           email: "operator@gmail.com",
           role: "operator"
         },
         {
+          id: 4,
           username: "dino",
           password: "password",
           email: "diner@gmail.com",

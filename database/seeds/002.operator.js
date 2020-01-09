d79f9cdeb83eabab48f4cdb8e7f497d544725cad
Jacqueline Omollo <jacquelineOmolloo@gmail.com>
2020-01-08 22:57:21 -0500

    commit

--- database/seeds/002.operator.js
+++ database/seeds/002.operator.js
@@ -6,19 +6,16 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex("operator").insert([
         {
-          id: 1,
           name: "Jackie",
           trucksOwn: "Burger Town1 and Burger Town 2",
           users_id: 1
         },
         {
-          id: 2,
           name: "Tom and Jerry",
           trucksOwn: "Burger Town3 and Burger Town 4",
           users_id: 3
         },
         {
-          id: 3,
           name: "Peter Pan",
           trucksOwn: "Taco Town 2 own Taco Town 3",
           users_id: 2
